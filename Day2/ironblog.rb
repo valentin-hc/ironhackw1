eebc2755bd22f6c7f4d165d35603556adcfeaa55
valentin-hc <valentin.hecqcauquil@gmail.com>
2015-10-13 18:17:44 +0200

    Add ironblog exercise

--- Day2/ironblog.rb
+++ Day2/ironblog.rb
@@ -1,8 +1,11 @@
 require "date"
+require "colorize"
 
 class Blog
+	attr_accessor :first_page
 	def initialize
 		@container = []
+		@first_page = 0
 	end
 	def add_post(post)
 		@container << post
@@ -13,26 +16,50 @@ class Blog
 		end
 		sorted
 	end
-	def split_by_page
+	def show_three_posts
 		sort_posts.each_with_index {|post, index|
-			if index/3 == 0
+			if index/3 == @first_page
 				puts post.title
+				puts "*************"
+				puts post.text
+				puts "Posted on #{post.date}"
 			end
 		}
-		
-	end
-	
+		puts @first_page
+		next_page
 
-	def publish_front_page
-		sort_posts.each do |post|
-			puts post.title
-			puts "*************"
-			puts post.text
-			puts "Posted on #{post.date}"
+	end
+	def next_page
+		puts "(next, prev)"
+		paginate
+		go_next = gets.chomp
+		if go_next == "next" && @first_page < number_of_page-1
+			@first_page +=1
+			show_three_posts
+		elsif go_next == "prev" && @first_page > 0
+			@first_page -=1
+			show_three_posts
+		else
+			puts "Let's stay here."
 		end
 	end
-	def number_of_pages
+	
+	def current_page 
+		@first_page + 1
+	end
+	def number_of_page
 		@container.size%3 + 1
+	end
+	def paginate
+		last_page = @container.size%number_of_page + 1
+		pages = (1..last_page+1).to_a
+		pages.each do |page|
+			if page == current_page
+				print " #{page} ".green
+			else
+				print " #{page} "
+			end
+		end
 	end
 end
 
@@ -61,8 +88,8 @@ end
 blog = Blog.new
 post1 = Post.new("Post title 1", Date.new(2015,10,10), "Hello there")
 post2 = Post.new("Post title 2", Date.new(2015,10,15), "Hello there")
-post3 = Sponsored.new("Happy Post", Date.new(2015,10,12), "Hello there")
-post4 = Sponsored.new("Happy Post2", Date.new(2015,10,16), "Hello there")
+post3 = Sponsored.new("Post title 3", Date.new(2015,10,12), "Hello there")
+post4 = Sponsored.new("Post title 4", Date.new(2015,10,16), "Hello there")
 
 
 blog.add_post(post1)
@@ -70,6 +97,14 @@ blog.add_post(post2)
 blog.add_post(post3)
 blog.add_post(post4)
 
+
+puts blog.number_of_page
 #blog.publish_front_page
 #puts 6/3
-blog.split_by_page
+#puts blog.first_page/3
+blog.show_three_posts
+#blog.next_page
+#puts blog.first_page
+
+#posts[0..2]
+
